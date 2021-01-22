d7e11c83b6b20309cedadb3d64d1aea6be581d26
Scott Louvau <slouvau@hotmail.com>
2021-01-22 15:00:53 -0800

    Make Stream disposal controllable.   Tried in-memory only (minimal difference).

--- src/LZ4s/LZ4s.Client/CompressionTester.cs
+++ src/LZ4s/LZ4s.Client/CompressionTester.cs
@@ -15,6 +15,7 @@ namespace LZ4s.Client
             Directory.CreateDirectory(outPath);
 
             ConsoleTable table = new ConsoleTable(
+                new TableCell("Name"),
                 new TableCell("U. Size", Align.Right),
                 new TableCell("C. Size", Align.Right),
                 new TableCell("Ratio", Align.Right),
@@ -28,26 +29,38 @@ namespace LZ4s.Client
                 string compressedFilePath = Path.Combine(compressedPath, file.Name + "." + extension);
                 string outFilePath = Path.Combine(outPath, file.Name);
 
-                Stopwatch cw = Stopwatch.StartNew();
-                Lz4sStream.Compress(file.FullName, compressedFilePath, buffer);
-                cw.Stop();
+                // To buffer in memory only. Minimal performance difference observed.
+                //using (Stream source = LoadToMemory(file.FullName))
+                //using (Stream compressed = new MemoryStream((int)file.Length))
+                //using (Stream uncompressed = new MemoryStream((int)file.Length))
 
-                FileInfo compressedFile = new FileInfo(compressedFilePath);
+                using (Stream source = File.OpenRead(file.FullName))
+                using (Stream compressed = File.Create(compressedFilePath))
+                using (Stream uncompressed = File.Create(outFilePath))
+                {
+                    Stopwatch cw = Stopwatch.StartNew();
+                    Lz4sStream.Compress(extension, source, compressed, buffer, closeStream: false);
+                    cw.Stop();
 
-                Stopwatch dw = Stopwatch.StartNew();
-                Lz4sStream.Decompress(compressedFilePath, outFilePath, buffer);
-                dw.Stop();
+                    long compressedLength = compressed.Length;
+                    compressed.Seek(0, SeekOrigin.Begin);
+                    
+                    Stopwatch dw = Stopwatch.StartNew();
+                    Lz4sStream.Decompress(extension, compressed, uncompressed, buffer, closeStream: false);
+                    dw.Stop();
 
-                bool matches = Lz4sStream.VerifyBytesEqual(file.FullName, outFilePath, out string errorMessage);
+                    bool matches = Lz4sStream.VerifyBytesEqual(source, uncompressed, file.FullName, outFilePath, out string errorMessage);
 
-                table.AppendRow(
-                    Format.Size(file.Length),
-                    Format.Size(compressedFile.Length),
-                    Format.Ratio(file.Length, compressedFile.Length),
-                    Format.Rate(file.Length, cw.Elapsed.TotalSeconds),
-                    Format.Rate(file.Length, dw.Elapsed.TotalSeconds),
-                    (matches ? "PASS" : $"FAIL {errorMessage}")
-                );
+                    table.AppendRow(
+                        file.Name,
+                        Format.Size(file.Length),
+                        Format.Size(compressedLength),
+                        Format.Ratio(file.Length, compressedLength),
+                        Format.Rate(file.Length, cw.Elapsed.TotalSeconds),
+                        Format.Rate(file.Length, dw.Elapsed.TotalSeconds),
+                        (matches ? "PASS" : $"FAIL {errorMessage}")
+                    );
+                }
             }
         }
 
@@ -57,6 +70,18 @@ namespace LZ4s.Client
 
             //tester.WriteHeader();
             //tester.RoundTripFile(new FileInfo(filePath), decompressIterations: decompressIterations);
+        }
+
+        private static Stream LoadToMemory(string filePath)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (Stream file = File.OpenRead(filePath))
+            {
+                file.CopyTo(ms);
+            }
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
         }
     }
 }
--- src/LZ4s/LZ4s/Lz4sStream.cs
+++ src/LZ4s/LZ4s/Lz4sStream.cs
@@ -7,31 +7,27 @@ namespace LZ4s
     {
         // TODO: Inherit stream, wrap Reader/Writer
 
-        private static IByteWriter BuildWriter(string filePath)
+        public static IByteWriter BuildWriter(string extension, Stream stream, bool closeStream = true)
         {
-            string extension = Path.GetExtension(filePath).ToLowerInvariant();
-
-            switch(extension)
+            switch (extension.TrimStart('.').ToLowerInvariant())
             {
-                case ".lz4s":
-                    return new Lz4sWriter(File.Create(filePath));
-                case ".lz4":
-                    return new Lz4Writer(File.Create(filePath));
+                case "lz4s":
+                    return new Lz4sWriter(stream, closeStream);
+                case "lz4":
+                    return new Lz4Writer(stream, closeStream);
                 default:
                     throw new NotImplementedException($"Lz4sStream does not know how to build writer for extension '{extension}'");
             }
         }
 
-        private static IByteReader BuildReader(string filePath)
+        public static IByteReader BuildReader(string extension, Stream stream, bool closeStream = true)
         {
-            string extension = Path.GetExtension(filePath).ToLowerInvariant();
-
-            switch (extension)
+            switch (extension.TrimStart('.').ToLowerInvariant())
             {
-                case ".lz4s":
-                    return new Lz4sReader(File.OpenRead(filePath));
-                case ".lz4":
-                    return new Lz4Reader(File.OpenRead(filePath));
+                case "lz4s":
+                    return new Lz4sReader(stream, closeStream);
+                case "lz4":
+                    return new Lz4Reader(stream, closeStream);
                 default:
                     throw new NotImplementedException($"Lz4sStream does not know how to build reader for extension '{extension}'");
             }
@@ -39,14 +35,22 @@ namespace LZ4s
 
         public static void Compress(string sourceFilePath, string compressedPath, byte[] buffer = null)
         {
-            Compress(File.OpenRead(sourceFilePath), BuildWriter(compressedPath), buffer);
+            using (Stream source = File.OpenRead(sourceFilePath))
+            using (IByteWriter writer = BuildWriter(Path.GetExtension(compressedPath), File.Create(compressedPath)))
+            {
+                Compress(source, writer, buffer);
+            }
+        }
+
+        public static void Compress(string extension, Stream source, Stream destination, byte[] buffer = null, bool closeStream = true)
+        {
+            Compress(source, BuildWriter(extension, destination, closeStream), buffer);
         }
 
         private static void Compress(Stream source, IByteWriter writer, byte[] buffer = null)
         {
             buffer ??= new byte[Constants.BufferSize];
 
-            using (source)
             using (writer)
             {
                 while (true)
@@ -61,14 +65,26 @@ namespace LZ4s
 
         public static void Decompress(string compressedPath, string destinationFilePath, byte[] buffer = null)
         {
-            Decompress(BuildReader(compressedPath), File.Create(destinationFilePath), buffer);
+            using (IByteReader reader = BuildReader(Path.GetExtension(compressedPath), File.OpenRead(compressedPath)))
+            using (Stream destination = File.Create(destinationFilePath))
+            {
+                Decompress(reader, destination, buffer);
+            }
+        }
+
+        public static void Decompress(string extension, Stream source, Stream destination, byte[] buffer = null, bool closeStream = true)
+        {
+            Decompress(
+                BuildReader(extension, source, closeStream),
+                destination,
+                buffer
+            );
         }
 
         private static void Decompress(IByteReader reader, Stream destination, byte[] buffer = null)
         {
             buffer ??= new byte[Constants.BufferSize];
 
-            using (destination)
             using (reader)
             {
                 while (true)
@@ -83,47 +99,47 @@ namespace LZ4s
 
         public static bool VerifyBytesEqual(string expectedFilePath, string actualFilePath, out string errorMessage)
         {
-            return VerifyBytesEqual(File.OpenRead(expectedFilePath), File.OpenRead(actualFilePath), expectedFilePath, actualFilePath, out errorMessage);
+            using (Stream expected = File.OpenRead(expectedFilePath))
+            using (Stream actual = File.OpenRead(actualFilePath))
+            {
+                return VerifyBytesEqual(expected, actual, expectedFilePath, actualFilePath, out errorMessage);
+            }
         }
 
         public static bool VerifyBytesEqual(Stream expected, Stream actual, string expectedName, string actualName, out string errorMessage)
         {
             errorMessage = null;
 
-            using (expected)
-            using (actual)
+            expected.Seek(0, SeekOrigin.Begin);
+            actual.Seek(0, SeekOrigin.Begin);
+
+            long position = 0;
+            byte[] bufferExpected = new byte[Constants.BufferSize];
+            byte[] bufferActual = new byte[bufferExpected.Length];
+
+            while (true)
             {
-                expected.Seek(0, SeekOrigin.Begin);
-                actual.Seek(0, SeekOrigin.Begin);
+                int expectedLength = expected.Read(bufferExpected, 0, bufferExpected.Length);
+                int actualLength = actual.Read(bufferActual, 0, bufferActual.Length);
 
-                long position = 0;
-                byte[] bufferExpected = new byte[Constants.BufferSize];
-                byte[] bufferActual = new byte[bufferExpected.Length];
-
-                while (true)
+                if (expectedLength != actualLength)
                 {
-                    int expectedLength = expected.Read(bufferExpected, 0, bufferExpected.Length);
-                    int actualLength = actual.Read(bufferActual, 0, bufferActual.Length);
+                    errorMessage = $"Error: Data ended at {(position + actualLength):n0} bytes, but {expected.Length:n0} total bytes expected.";
+                    return false;
+                }
 
-                    if (expectedLength != actualLength)
+                if (expectedLength == 0) { break; }
+
+                for (int i = 0; i < expectedLength; ++i)
+                {
+                    if (bufferExpected[i] != bufferActual[i])
                     {
-                        errorMessage = $"Error: Data ended at {(position + actualLength):n0} bytes, bute {expected.Length:n0} total bytes expected.";
+                        errorMessage = $"Error: At {position + i:n0}, expected {bufferExpected[i]} but read {bufferActual[i]}.";
                         return false;
                     }
-
-                    if (expectedLength == 0) { break; }
-
-                    for (int i = 0; i < expectedLength; ++i)
-                    {
-                        if (bufferExpected[i] != bufferActual[i])
-                        {
-                            errorMessage = $"Error: At {position + i:n0}, expected {bufferExpected[i]} but read {bufferActual[i]}.";
-                            return false;
-                        }
-                    }
-
-                    position += expectedLength;
                 }
+
+                position += expectedLength;
             }
 
             return true;
